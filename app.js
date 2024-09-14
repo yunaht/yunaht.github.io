991dc4ac5a9c885e043f2346ed76bd6d56ee8300
yunaht <thanushany1@gmail.com>
2024-09-14 23:51:17 +0100

    first website ever!

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+const images=document.querySelectorAll("img");
+
+for(const image of images){
+fetch(https://dog.ceo/api/breeds/image/random])
+    .then(response => response.json())
+    .then(data => image.src = data.message)
+}
+
+})
